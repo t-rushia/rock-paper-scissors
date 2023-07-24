b83845e9d4689379b013f2a24ca621c420512fe9
Thomas Rushia <thomas.rushia@outlook.com>
2023-07-24 08:30:32 -0400

    Added getComputerChoice function which randomly chooses rock, paper, scissors and assigns it to the computerChoice variable

--- index.js
+++ index.js
@@ -6,3 +6,12 @@ const scissorsChoice = document.querySelector("#scissors-el");
 let playerScore = document.querySelector("#playerscore-el");
 let computerScore = document.querySelector("#computerscore-el");
 let gameResults = document.querySelector("#gameresults-el");
+
+function getComputerChoice() {
+  let options = ["Rock", "Paper", "Scissors"];
+  computerChoice = options[Math.floor(Math.random() * options.length)];
+  return computerChoice;
+}
+
+getComputerChoice();
+console.log(computerChoice);
